f22b0977aea39cfba6b722b40dcab4b0baddbe53
Tøbé Çhükwû <astrolix278@gmail.com>
2025-08-08 14:56:33 +0100

    craeted a repo with the starter folders

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const NAvList = [
+    {
+        Navitem: 'Fashion',
+    },
+    {
+        Navitem: 'Food',
+    },
+    {
+        Navitem: 'Art',
+    },
+    {
+        Navitem: 'Beauty',
+    },
+    {
+        Navitem: 'Crafts',
+    },
+    {
+        Navitem: 'Tailoring',
+    }
+]
+
+const Nav = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Nav
